e896b49c0e4091bc207a697a6eafbab363060014
Luke Colcott <git@github.com:lukeylunchbox/test.git>
2018-04-25 16:38:17 +1000

    Add scaffold - Books model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+newbooks = Book.create([
+    {title: "To the Lighthouse", author: "Virginia Woolf", description: "Lily Briscoe is a guest at the Ramsays’ busy holiday house and in love with all of them"},
+    {title: "The Horse’s Mouth", author: "Joyce Cary", description: "Gulley Jimson is 67 and just out of jail."},
+    {title: "The Recognitions", author: "William Gaddis", description: "This vast triptych of a novel is filled with artists, critics, plagiarists, failures, charlatans and frauds."},
+    {title: "Portrait of a Man", author: "Georges Perec", description: "Gaspard Winckler, no longer satisfied after 12 years in the business, becomes obsessed with the impossible task of forging an authentic Antonello da Messina."},
+    {title: "The True Deceiver", author: "Tove Jansson", description: "Anna Aemelin paints small watercolours of the forest floor, waiting for the snow to thaw before setting out into the dense wall of trees behind her house every spring."}
+])
---
+++ spec/views/books/new.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "books/new", type: :view do
+  before(:each) do
+    assign(:book, Book.new(
+      :title => "MyString",
+      :author => "MyString",
+      :description => "MyText",
+      :user => nil
+    ))
+  end
+
+  it "renders new book form" do
+    render
+
+    assert_select "form[action=?][method=?]", books_path, "post" do
+
+      assert_select "input[name=?]", "book[title]"
+
+      assert_select "input[name=?]", "book[author]"
+
+      assert_select "textarea[name=?]", "book[description]"
+
+      assert_select "input[name=?]", "book[user_id]"
+    end
+  end
+end
